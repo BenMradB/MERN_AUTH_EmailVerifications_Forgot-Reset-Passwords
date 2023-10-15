378319bbf19e26784afa22865df6e9378612433d
BenMradB <bilelbenmrad2001@gmail.com>
2023-10-15 18:28:28 +0100

    Changing the file name nodemailerTransporter.js -> sendMails.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 import User from "../models/user.model.js";
 import UserVerification from "../models/user.verification.js";
 import generateToken from "../utils/generateToken.util.js";
-import { sendMails } from "../utils/nodemailerTransporter.js";
+import { sendMails } from "../utils/sendMails.js";
 
 // @Route   POST /api/users/auth
 // @Access  Public
